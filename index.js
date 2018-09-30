ce72e008d4639cf0b7b7d5592948f11bcf5b1c92
JackFly26 <jackfleishman@outlook.com>
2018-09-30 10:14:02 -0400

    Create base

--- index.js
+++ index.js
@@ -1,5 +1,59 @@
 const Discord = require('discord.js');
-const {token} = require('./token.json')
-const Client = new Discord.Client();
-Client.login(token);
-module.exports.Client = Client;
+const fs = require('fs');
+const {
+    token
+} = require('./token.json');
+const client = new Discord.Client();
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+let servers;
+try {
+    servers = JSON.parse(fs.readFileSync('servers.json'));
+} catch (error) {
+    servers = {};
+}
+client.on('ready', () => {
+    client.user.setActivity('Botting...');
+});
+client.on('message', async (message) => {
+    if (!message.guild) {
+        if (!servers[`channel${message.channel.id}`]) {
+            servers[`channel${message.channel.id}`] = {
+                'prefix': ';'
+            };
+        }
+    } else if (!servers[message.guild.id]) {
+        servers[message.guild.id] = {
+            'playing': false,
+            'prefix': ';',
+            'queue': []
+        };
+    }
+    let prefix;
+    try {
+        ({prefix} = servers[message.guild.id].prefix);
+    } catch (error) {
+        ({prefix} = servers[`channel${message.channel.id}`].prefix);
+    }
+    if (message.content.startsWith(prefix) && !message.author.bot) {
+        const args = message.content.slice(prefix.length).split(/\s+/u);
+        const commandName = args.shift().toLowerCase();
+        const command = client.commands.get(commandName) || client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
+        if (!command) {
+            return;
+        }
+        try {
+            await command.execute(message, args);
+            if (message.channel.type === 'text' && command.name === 'count') {
+                message.delete();
+            }
+        } catch (error) {
+            message.reply(`there was an error trying to execute that command!: ${error.message}`);
+        }
+    }
+});
+client.login(token);
+module.exports.client = client;
